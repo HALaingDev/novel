0e699d6ccbeadca5bf5422d1c31eda8869bbf682
HALaing <61649323+HALaingDev@users.noreply.github.com>
2020-06-03 17:05:25 +0630

    Add files via upload

--- app.js
+++ app.js
@@ -12,16 +12,25 @@ function begin() {
     ငါအဲ့လိုရေရွတ်လိုက်မိတယ် <br>
     အိမ်ကထွက်လာတော့ပတ်ဝန်းကျင်ကအနည်းငယ်ပူလောင်နေတယ်<br>
     သွားမဲ့နေရာကတောင်ပေါ်မြို့လေးဆိုတော့ဟိုရောက်ရင်တော့အေးမယ်လို့မျှော်လင့်ရတာဘဲ<br>
-    သက်ပြင်းတစ်ချက်မသိမသာချလိုက်မိတယ်</p><br>
-    <button class='btn1' id = 'school' onclick='school.result();'>ကားနဲ့သွားမယ်<br>(၁၂နာရီကြာမယ်)</button>
+    သက်ပြင်းတစ်ချက်မသိမသာချလိုက်မိတယ်</p><br>    
+    <button class='btn1' id = 'school' onclick='car.result();'>ကားနဲ့သွားမယ်<br>(၁၂နာရီကြာမယ်)</button>
     <button class='btn2' id='home' onclick='home.result();'>ရထားနဲ့သွားမယ်<br>(၁ရက်ခွဲကြာမယ်)</button>`;
 }
-let school = {
+let car = {
     result: function () {
-        text.innerHTML = `< p > What subject is been teaching now ?</p >
-        <button onclick='stop();'>Myanmar</button>
-        <button onclick='stop();'>English</button>
-        <button onclick='begin();'>Ask me again.</button>`;
+        text.innerHTML = `<p>သွားရမယ့်မြို့ကအိမ်ခြေနည်းလို့လားမသိဘူး
+        ကားပေါ်မှာလူနည်းနည်းဘဲပါတယ်<br>
+        ကားလေးကတရွေ့ရွေ့နဲ့လယ်ကွင်း​တွေ,တောင်တက်လမ်းတွေ , ထင်းရှူးတောတွေကိုတရွေ့ရွေ့နဲ့ဖြတ်လာတယ်<br>
+        မြူခိုးတွေဆိုင်းနေတဲ့အဝေးမြို့လေးကိုလှမ်းတောင်မြင်နေရပြီ
+        မြို့ကိုရောက်တော့ပတ်ဝန်းကျင်ကိုသတိထားလိုက်မိတယ်
+        ကားပေါ်မှာကားသမားနဲ့ငါဘဲကျန်တော့တယ်<br>ခရီးသည်တွေလမ်းမှာဆင်းသွားတယ်ထင်ပါရဲ့<br>
+        ကားပေါ်ကဆင်းတော့လေအေးတစ်ချက်မျက်နှာနားကိုဖြတ်တိုက်သွားတယ်
+        အေးစက်စက်ပတ်ဝန်းကျင်ရဲ့အငွေ့အသက်ကကျောထဲထိစိမ့်သွားသလိုတောင်ခံစားရတယ်</p><br>    
+    <button class='btn1' id = 'school' onclick='car.looking();'>ပတ်ဝန်းကျင်ကို<br>ကြည့်မယ်</button>
+    <button class='btn2' id='home' onclick='home.result();'>​ဆေးရုံတန်းသွားမယ်</button>`;
+    },
+    looking: function () {
+
     }
 }
 let home = {
